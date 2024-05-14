8a3a791ab2eb31ba9c2a5f0b1e59cfb61d1f6223
kkh5535 <kkh5535@naver.com>
2024-05-14 22:14:23 +0900

    Test: SensorType 테스트

--- src/test/java/live/databo3/sensor/device/service/DeviceServiceTest.java
+++ src/test/java/live/databo3/sensor/device/service/DeviceServiceTest.java
@@ -22,7 +22,6 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
