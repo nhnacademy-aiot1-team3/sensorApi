f5561fa97aeb9199259963310a838ff78ac34971
kkh5535 <kkh5535@naver.com>
2024-05-16 09:56:49 +0900

    Fix: 센서 자동 추가 uri 수정

--- src/main/java/live/databo3/sensor/rule_engine/controller/RuleEngineController.java
+++ src/main/java/live/databo3/sensor/rule_engine/controller/RuleEngineController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/sensor/")
+@RequestMapping
 public class RuleEngineController {
     private final RuleEngineService ruleEngineService;
 
