c54bfbc1a98415ef30b5e24f31096cda0ccc9b66
kkh5535 <kkh5535@naver.com>
2024-05-18 17:23:54 +0900

    Feat: controllerAdvice Test

--- src/main/java/live/databo3/sensor/organization/repository/OrganizationRepository.java
+++ src/main/java/live/databo3/sensor/organization/repository/OrganizationRepository.java
@@ -4,9 +4,7 @@ import live.databo3.sensor.organization.dto.OrganizationId;
 import live.databo3.sensor.organization.dto.OrganizationName;
 import live.databo3.sensor.organization.entity.Organization;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface OrganizationRepository extends JpaRepository<Organization, Integer> {
@@ -15,7 +13,4 @@ public interface OrganizationRepository extends JpaRepository<Organization, Inte
     Optional<OrganizationName> findOrganizationNameByOrganizationId(Integer organizationId);
 
     Optional<OrganizationId> findOrganizationIdByOrganizationName(String organizationName);
-
-    @Query("SELECT o.organizationId FROM Organization o")
-    List<Integer> getAllIds();
 }
