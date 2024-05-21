3d44207a1cfc02b1879fff58e1cfad445a2f9168
kkh5535 <kkh5535@naver.com>
2024-05-21 17:29:44 +0900

    Feat: getConfig 기능 추가

--- src/main/java/live/databo3/sensor/value_config/service/ValueConfigService.java
+++ src/main/java/live/databo3/sensor/value_config/service/ValueConfigService.java
@@ -1,5 +1,6 @@
 package live.databo3.sensor.value_config.service;
 
+import live.databo3.sensor.value_config.dto.ConfigsDto;
 import live.databo3.sensor.value_config.dto.ValueConfigDto;
 import live.databo3.sensor.value_config.dto.ValueConfigRequest;
 import live.databo3.sensor.value_config.dto.ValueConfigResponse;
@@ -12,4 +13,5 @@ public interface ValueConfigService {
     void deleteValueConfig(Integer organizationId, String sensorSn, Integer sensorTypeId, Long valueConfigNumber);
     ValueConfigResponse getValueConfig(Integer organizationId, String sensorSn, Integer sensorTypeId, Long valueConfigNumber);
     List<ValueConfigDto> getValueConfigListByOrganizationId(Integer organizationId);
+    ConfigsDto getConfig(Integer organizationId, String sensorSn, Integer sensorTypeId);
 }
